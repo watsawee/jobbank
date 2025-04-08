7cbedca84e85b1dd342dca9ee18ed3ce6a56b874
Watsawee <watsawee@gmail.com>
2025-04-08 19:09:57 +0700

    initial commit - add all files

---
+++ payRateCal.js
@@ -0,0 +1,10 @@
+function payRateCalculate() {
+  let workDays = (document.getElementById("workDays").value).trim();
+  let selectObj = document.getElementById("year");
+  let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
+  //alert("yearOfStudy = "+yearOfStudy);
+  
+  return 0;
+}
+
+
---
+++ validate.js
@@ -0,0 +1,35 @@
+function checkNID() {
+  let nid = (document.getElementById("nid").value).trim();
+  if(isNaN(nid)){
+	return false;
+  }else{
+	return true;
+  }
+}
+
+function checkWorkDays() {
+  let num = (document.getElementById("workDays").value).trim();
+  if (isNaN(num)) {
+    return false;
+  } else {
+	return true;
+  }
+}
+
+function validateForm(){
+	if(!checkNID()){
+	  alert("Invalid value for National ID!");
+	  document.getElementById("nid").focus();
+	  return false;
+	}else{
+		if(!checkWorkDays()){
+		  alert("Invalid value for days of work!");
+		  document.getElementById("workDays").focus();
+		  return false;
+		}else{
+			total = payRateCalculate();
+			alert("Your weekly pay rate is "+total+" THB");
+			return false;
+		}
+	}
+}
