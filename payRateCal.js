94c7a7b8d9cfc7723cbe4a10a0d96d1176326bda
Chaianan <Chaianansangsurakul@gmailcom>
2025-04-09 11:04:28 +0700

    calculate weekly pay rate

--- payRateCal.js
+++ payRateCal.js
@@ -4,7 +4,29 @@ function payRateCalculate() {
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
   //alert("yearOfStudy = "+yearOfStudy);
   
-  return 0;
-}
+  const workHours = 1;
 
+  let hourlyRate = 0;
+
+  // กำหนดอัตราค่าจ้างต่อชั่วโมงตามปี
+  switch (yearOfStudy) {
+    case 1:
+      hourlyRate = 55;
+      break;
+    case 2:
+      hourlyRate = 65;
+      break;
+    case 3:
+      hourlyRate = 75;
+      break;
+    case 4:
+    default:
+      hourlyRate = 85;
+      break;
+  }
+
+  // คำนวณค่าตอบแทนรายสัปดาห์
+  const totalPay = workDays * workHours * hourlyRate;
+  return totalPay;
+}
 
