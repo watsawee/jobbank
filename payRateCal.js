26530ea135eede1cd800ad67c5bcc561fa0ae2b3
Siriyakorn <siriyakornice12@gmail.com>
2025-04-09 12:21:51 +0700

    Fix validation and implement weekly pay rate calculation

--- payRateCal.js
+++ payRateCal.js
@@ -1,10 +1,27 @@
 function payRateCalculate() {
-  let workDays = (document.getElementById("workDays").value).trim();
+  let workDays = parseInt(document.getElementById("workDays").value);
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
-  
-  return 0;
-}
+
+  const hoursPerDay = 12; 
+ 
+  let hourlyRate;
+ 
+  if (yearOfStudy === 1) {
+    hourlyRate = 55;
+  } else if (yearOfStudy === 2) {
+    hourlyRate = 65;
+  } else if (yearOfStudy === 3) {
+    hourlyRate = 75;
+  } else if (yearOfStudy === 4) {
+    hourlyRate = 85;
+  }
+ 
+  let weeklyPayRate = hourlyRate * workDays * hoursPerDay;
+ 
+  alert("Your weekly pay rate is " + weeklyPayRate + " THB");
+ 
+  return weeklyPayRate;
+  }
 
 
