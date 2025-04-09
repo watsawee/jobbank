72a592b41812589f92f9243021b340e102cfd5af
Pornphetcharat <2310511101032@live4.utcc.ac.th>
2025-04-09 10:54:46 +0700

    Commit#2:Add function for Id to be exactly 13 digits

--- validate.js
+++ validate.js
@@ -1,12 +1,14 @@
 function checkNID() {
-  let nid = (document.getElementById("nid").value).trim();
-  if(isNaN(nid)){
-	return false;
-  }else{
-	return true;
+	let nid = (document.getElementById("nid").value).trim();
+	
+	if (nid.length !== 13 || isNaN(nid) || nid === "") {
+		return false;  
+	  } else {
+		return true;  
+	}
   }
-}
-
+  
+  
 function checkWorkDays() {
   let num = (document.getElementById("workDays").value).trim();
   if (isNaN(num)) {
@@ -18,18 +20,18 @@ function checkWorkDays() {
 
 function validateForm(){
 	if(!checkNID()){
-	  alert("Invalid value for National ID!");
+	  alert("Invalid National ID! Please enter exactly 13 digits.");
 	  document.getElementById("nid").focus();
 	  return false;
-	}else{
-		if(!checkWorkDays()){
-		  alert("Invalid value for days of work!");
-		  document.getElementById("workDays").focus();
-		  return false;
-		}else{
-			total = payRateCalculate();
-			alert("Your weekly pay rate is "+total+" THB");
-			return false;
-		}
+	} else {
+	  if(!checkWorkDays()){
+		alert("Invalid value for days of work!");
+		document.getElementById("workDays").focus();
+		return false;
+	  } else {
+		total = payRateCalculate();
+		alert("Your weekly pay rate is "+total+" THB");
+		return false;
+	  }
 	}
-}
+  }
