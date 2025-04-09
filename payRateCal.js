596167034857430f9ab8d3b18d43958fe9c5b5b6
Pisit <pisitpathomsuriyaporn@gmail.com>
2025-04-09 11:08:36 +0700

    Calculate the weekly pay rate of Ang Ing and ask for the information of the grade that the student is studying

--- payRateCal.js
+++ payRateCal.js
@@ -1,10 +1,16 @@
 function payRateCalculate() {
-  let workDays = (document.getElementById("workDays").value).trim();
-  let selectObj = document.getElementById("year");
-  let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
+  let workDays = parseInt(document.getElementById("workDays").value.trim());
+  let yearOfStudy = parseInt(document.getElementById("year").value);
+  let hourlyRate = 0;
   
-  return 0;
+  switch (yearOfStudy) {
+    case 1: hourlyRate = 55; break;
+    case 2: hourlyRate = 65; break;
+    case 3: hourlyRate = 75; break;
+    case 4: hourlyRate = 85; break;
+    default: hourlyRate = 0;
+  }
+  return workDays * hourlyRate;
 }
 
 
