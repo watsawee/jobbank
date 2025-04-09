878a60cfd217fbfe05883814e6b6b2926e72f1e7
Jesadaporn Mantree <2310511101003@live4.utcc.ac.th>
2025-04-09 11:23:28 +0700

    Commit#4 - add more function calcurate

--- validate.js
+++ validate.js
@@ -1,31 +1,33 @@
 function checkNID() {
-  let nid = (document.getElementById("nid").value).trim();
-  return /^\d{13}$/.test(nid);
+	let nid = (document.getElementById("nid").value).trim();
+	return /^\d{13}$/.test(nid);
 }
 
 function checkWorkDays() {
-  let num = (document.getElementById("workDays").value).trim();
-  let days = Number(num);
-  if (!/^\d+$/.test(num) || days < 1 || days > 6) {
-    return false;
-  } else {
-    return true;
-  }
+	let num = (document.getElementById("workDays").value).trim();
+	let days = Number(num);
+	if (!/^\d+$/.test(num) || days < 1 || days > 6) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
-function validateForm(){
-	if(!checkNID()){
-	  alert("Invalid value for National ID!");
-	  document.getElementById("nid").focus();
-	  return false;
-	}else{
-		if(!checkWorkDays()){
-		  alert("Invalid value for days of work!");
-		  document.getElementById("workDays").focus();
-		  return false;
-		}else{
-			total = payRateCalculate();
-			alert("Your weekly pay rate is "+total+" THB");
+function validateForm() {
+	if (!checkNID()) {
+		alert("Invalid value for National ID!");
+		document.getElementById("nid").focus();
+		return false;
+	} else {
+		if (!checkWorkDays()) {
+			alert("Invalid value for days of work!");
+			document.getElementById("workDays").focus();
+			return false;
+		} else {
+			let total = payRateCalculate();
+			if (total !== null) {
+				alert("Your weekly pay rate is " + total + " THB");
+			}
 			return false;
 		}
 	}
