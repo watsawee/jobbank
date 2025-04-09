a1eaded7d64935b3c5bf735dc4df74000bf9ce71
Sirinphat Darawankul <2410511101067utcc@gmail.com>
2025-04-09 10:34:53 +0700

    adding feature#1 - add required form and NationId is only 13 number

--- validate.js
+++ validate.js
@@ -1,35 +1,35 @@
 function checkNID() {
-  let nid = (document.getElementById("nid").value).trim();
-  if(isNaN(nid)){
-	return false;
-  }else{
-	return true;
-  }
-}
-
-function checkWorkDays() {
-  let num = (document.getElementById("workDays").value).trim();
-  if (isNaN(num)) {
-    return false;
-  } else {
-	return true;
-  }
-}
-
-function validateForm(){
-	if(!checkNID()){
-	  alert("Invalid value for National ID!");
-	  document.getElementById("nid").focus();
+	let nid = (document.getElementById("nid").value).trim();
+	if(isNaN(nid) || nid.length !== 13){
 	  return false;
 	}else{
-		if(!checkWorkDays()){
-		  alert("Invalid value for days of work!");
-		  document.getElementById("workDays").focus();
-		  return false;
-		}else{
-			total = payRateCalculate();
-			alert("Your weekly pay rate is "+total+" THB");
-			return false;
-		}
+	  return true;
 	}
-}
+  }
+  
+  function checkWorkDays() {
+	let num = (document.getElementById("workDays").value).trim();
+	if (isNaN(num)) {
+	  return false;
+	} else {
+	  return true;
+	}
+  }
+  
+  function validateForm(){
+	  if(!checkNID()){
+		alert("Invalid value for National ID! it not be 13");
+		document.getElementById("nid").focus();
+		return false;
+	  }else{
+		  if(!checkWorkDays()){
+			alert("Invalid value for days of work!");
+			document.getElementById("workDays").focus();
+			return false;
+		  }else{
+			  total = payRateCalculate();
+			  alert("Your weekly pay rate is "+total+" THB");
+			  return false;
+		  }
+	  }
+  }
