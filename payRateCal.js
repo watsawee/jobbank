804e639bad33962a9c72afbfbe6889450e9cb044
Varatchaya <2310511101063@live4.utcc.ac.th>
2025-04-09 11:28:49 +0700

    Adding Calculate in funtion payRateCalculate in payRateCal.js

--- payRateCal.js
+++ payRateCal.js
@@ -1,10 +1,30 @@
 function payRateCalculate() {
+  
   let workDays = (document.getElementById("workDays").value).trim();
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
   
-  return 0;
+  let hourlyRate;
+  
+  switch(yearOfStudy) {
+    case 1:
+      hourlyRate = 55;
+      break;
+    case 2:
+      hourlyRate = 65; 
+      break;
+    case 3:
+      hourlyRate = 75; 
+      break;
+    case 4:
+      hourlyRate = 85; 
+      break;
+    default:
+      hourlyRate = 0;  
+  }
+
+ 
+  let weeklyPayRate = hourlyRate * workDays * 8 + " In case work 8 hour/day"; 
+  
+  return weeklyPayRate;
 }
-
-
