ba2a3a1c13989a772c3c12088406f08a8ebdafd8
Nonpawit <2310511101105@live4.utcc.ac.th>
2025-04-09 11:39:50 +0700

    four commit : fix cal money

--- payRateCal.js
+++ payRateCal.js
@@ -1,9 +1,27 @@
 function payRateCalculate() {
-  let workDays = (document.getElementById("workDays").value).trim();
+  let workDays = parseInt(document.getElementById("workDays").value.trim());
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
-  
-  return 0;
-}
 
+  let ratePerHour = 0;
+
+  switch (yearOfStudy) {
+      case 1:
+          ratePerHour = 55;
+          break;
+      case 2:
+          ratePerHour = 65;
+          break;
+      case 3:
+          ratePerHour = 75;
+          break;
+      case 4:
+          ratePerHour = 85;
+          break;
+      default:
+          ratePerHour = 0;
+  }
+
+  let totalPay = ratePerHour * 6 * workDays; 
+  return totalPay;
+}
