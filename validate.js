df1f3022ff472caaa4ef390e8c43225dda9ac831
Aimlessisland <gamee1793@gmail.com>
2025-04-09 10:56:39 +0700

    misunderstand add all requied

--- validate.js
+++ validate.js
@@ -7,24 +7,6 @@ function checkNID() {
   }
 }
 
-function checkfname(){
-	let firstName = (document.getElementById("fname").value).trim();
-	if(isNaN(firstName)){
-		return false;
-	}else{
-		return true;
-  }
-}
-
-function checkLname(){
-	let lastName = (document.getElementById("lname").value).trim();
-	if(isNaN(lastName)){
-		return false;
-	}else{
-		return true;
-  }
-}
-
 function checkWorkDays() {
   let num = (document.getElementById("workDays").value).trim();
   if (isNaN(num)) {
@@ -46,20 +28,6 @@ function validateForm(){
 	}
 	if(userInput < 13 || userInput > 13 ){
 		alert("Need 13 digit for National ID!");
-		return false;
-		
-	}
-	
-	//#####################################################
-	
-	if(!checkfname()){
-		alert("Invalid value for First Name");
-		document.getElementById("fname").focus();
-		return false;
-	}
-	if(!checkLname()){
-		alert("Invalid value for Last Name");
-		document.getElementById("lname").focus();
 		return false;
 	}
 	
