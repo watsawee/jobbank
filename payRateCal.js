83edc629faa3c9d330d8520137f5389abb192539
Renne <kirahlynnst@gmail.com>
2025-04-09 10:43:33 +0700

    swtch_payment

--- payRateCal.js
+++ payRateCal.js
@@ -2,7 +2,14 @@ function payRateCalculate() {
   let workDays = (document.getElementById("workDays").value).trim();
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
+  
+  switch (yearOfStudy) {
+    case 1: return workDays * 55;
+    case 2: return workDays * 65;
+    case 3: return workDays * 75;
+    case 4: return workDays * 85;
+    default: return 0;
+  }
   
   return 0;
 }
