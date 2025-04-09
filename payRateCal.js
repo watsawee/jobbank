8b1b05a78eedb04c0a26a9691928f6eec1d63b5c
JakkapongPengtee <bunpeng3@gmail.com>
2025-04-09 11:32:16 +0700

    final 1

--- payRateCal.js
+++ payRateCal.js
@@ -2,7 +2,22 @@ function payRateCalculate() {
   let workDays = (document.getElementById("workDays").value).trim();
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
+  let perRate = 0;
+
+  if(yearOfStudy == 0){
+    perRate = 55;
+  }
+  if(yearOfStudy == 0){
+    perRate = 65;
+  }
+  if(yearOfStudy == 0){
+    perRate = 75;
+  }
+  if(yearOfStudy == 0){
+    perRate = 85;
+  }
+
+  return workDays * perRate;
   
   return 0;
 }
