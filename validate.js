002ab79dd216988f8c0d9c289ea5359c6de24902
Aimlessisland <gamee1793@gmail.com>
2025-04-09 10:50:23 +0700

    Find out required bug to JS

--- validate.js
+++ validate.js
@@ -7,6 +7,24 @@ function checkNID() {
   }
 }
 
+function checkfname(){
+	let firstName = (document.getElementById("fname").value).trim();
+	if(isNaN(firstName)){
+		return false;
+	}else{
+		return true;
+  }
+}
+
+function checkLname(){
+	let lastName = (document.getElementById("lname").value).trim();
+	if(isNaN(lastName)){
+		return false;
+	}else{
+		return true;
+  }
+}
+
 function checkWorkDays() {
   let num = (document.getElementById("workDays").value).trim();
   if (isNaN(num)) {
@@ -17,11 +35,37 @@ function checkWorkDays() {
 }
 
 function validateForm(){
+	
+	let userInput = document.getElementById('nid').value.length;
+	console.log(userInput);
+	
 	if(!checkNID()){
 	  alert("Invalid value for National ID!");
 	  document.getElementById("nid").focus();
 	  return false;
-	}else{
+	}
+	if(userInput < 13 || userInput > 13 ){
+		alert("Need 13 digit for National ID!");
+		return false;
+		
+	}
+	
+	//#####################################################
+	
+	if(!checkfname()){
+		alert("Invalid value for First Name");
+		document.getElementById("fname").focus();
+		return false;
+	}
+	if(!checkLname()){
+		alert("Invalid value for Last Name");
+		document.getElementById("lname").focus();
+		return false;
+	}
+	
+	//######################################################
+	
+	else{
 		if(!checkWorkDays()){
 		  alert("Invalid value for days of work!");
 		  document.getElementById("workDays").focus();
