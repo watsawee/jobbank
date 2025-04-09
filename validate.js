877c624bc9d5e4af8e9e8447a3896aa05f302723
penpitcha <penpitchatong13@gmail.com>
2025-04-09 11:00:04 +0700

    lock gender

--- validate.js
+++ validate.js
@@ -1,20 +1,18 @@
 function checkNID() {
 	let nid = (document.getElementById("nid").value).trim();
-	// ตรวจสอบว่า nid เป็นตัวเลขทั้งหมดและไม่ว่าง
 	if (isNaN(nid) || nid === "") {
-	  return false; // ไม่ใช่ตัวเลขหรือกรอกช่องว่าง
+	  return false; 
 	} else {
-	  return true; // เป็นตัวเลข
+	  return true; 
 	}
   }
   
   function checkWorkDays() {
 	let num = (document.getElementById("workDays").value).trim();
-	// ตรวจสอบว่า num เป็นตัวเลขทั้งหมดและไม่ว่าง
 	if (isNaN(num) || num === "") {
-	  return false; // ไม่ใช่ตัวเลขหรือกรอกช่องว่าง
+	  return false; 
 	} else {
-	  return true; // เป็นตัวเลข
+	  return true; 
 	}
   }
   
@@ -22,16 +20,16 @@ function checkNID() {
 	if (!checkNID()) {
 	  alert("Invalid value for National ID! Please enter only numeric values.");
 	  document.getElementById("nid").focus();
-	  return false; // หยุดการส่งฟอร์ม
+	  return false; 
 	} else {
 	  if (!checkWorkDays()) {
 		alert("Invalid value for days of work! Please enter only numeric values.");
 		document.getElementById("workDays").focus();
-		return false; // หยุดการส่งฟอร์ม
+		return false; 
 	  } else {
 		total = payRateCalculate();
 		alert("Your weekly pay rate is " + total + " THB");
-		return false; // หยุดการส่งฟอร์ม
+		return false; 
 	  }
 	}
   }
